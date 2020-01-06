3c68ff5e8a65a207c638166e68fc418a81f74f8d
Christoph Wagner <github@pandawhisperer.net>
2020-01-06 10:11:07 -0800

    Add examples

---
+++ examples/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import YouTube from '../../src/index.svelte';
+
+	export let videoId;
+</script>
+
+<main>
+	<h1>Svelte-YouTube</h1>
+	<p>Embedding a single video:</p>
+	<YouTube {videoId} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ examples/src/main.js
@@ -0,0 +1,10 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		videoId: "M7lc1UVf-VE"
+	}
+});
+
+export default app;
